997e7a8720a0e6c0d2259c0dba187590577a08ed
zelbrium <zelbrium@gmail.com>
2016-02-13 21:51:31 -0500

    Added support for automatic DT* calculations through Electron.
    A compiled dtstar executable for Mac is temporarily available. No source code is currently provided, but that will be fixed soon.

--- js/FlippingSequence.js
+++ js/FlippingSequence.js
@@ -1,7 +1,141 @@
+//Copyright (C) 2016 Aidan Shafran
+//
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+//
+//The above copyright notice and this permission notice shall be included in
+//all copies or substantial portions of the Software.
+//
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//THE SOFTWARE.
+
 var OptionsPanel = document.getElementById("options-panel");
 var FlippingSequencePanel = document.getElementById("flipping-sequence-panel");
 
 var WasShowingCPTs = false;
+
+function VisualizeFlippingSequence()
+{
+	if(IsRunningElectron()) // If electron, run the flipping sequnece generator executable to generate the XML ourselves
+	{
+		GenerateAndLoadFlippingSequence();
+	}
+	else // If not, ask the user to upload a flipping sequence XML file
+	{
+		ShowMessageBox("Automatic DT* is unavailable when not running in Electron. You must select an XML output file from DT* manually.", null, function() {
+			LoadFlippingSequenceXMLFromFile();
+		});
+	}
+}
+
+function GenerateAndLoadFlippingSequence() // This function requires Electron
+{
+	var dtstar = require('dtstar');
+	
+	// Create GUI for selecting best and worse outcomes
+	var div = d3.select(document.createElement('div'));
+	div.append('p').text("Choose a 'better' and a 'worse' outcome; DT* will be used to find the flipping sequence between the two, if one exists.")
+	
+	var rootTable = div.append('center').append('table').style('margin-bottom', "1.3em");
+	
+	var ends = ['Better', 'Worse'];
+	
+	var topRow = rootTable.append('tr');
+	topRow.append('td'); // Empty cell
+	for(var i=0;i<ends.length;++i)
+		topRow.append('td').text(ends[i]);
+
+	for(var i=0;i<Graph.Nodes.length;++i)
+	{
+		var row = rootTable.append('tr');
+		row.append('td').append('p').text(Graph.Nodes[i].Name).style('margin', "0 5px 0 5px").style('text-align', 'left');
+		
+		for(var j=0;j<ends.length;++j)
+		{
+			var select = row.append('td').style('padding', "0 5px 0 5px").append('select').style('width', "100%");
+			select.attr("id", "flipseqselector_node" + i + "_end" + j);
+			
+			for(var k=0;k<Graph.Nodes[i].Domain.length;++k)
+				select.append('option').attr('value', Graph.Nodes[i].Domain[k]).text(Graph.Nodes[i].Domain[k]);
+		}
+	}
+	
+	// FOR SINGLE COMLUMN
+	// for(var i=0;i<ends.length;++i)
+	// {
+	// 	var rootTableCol = rootTableRow.append('td');
+	// 	
+	// 	rootTableCol.append('p').text(ends[i].name + " outcome").style('margin-bottom', 0);
+	// 	var table = rootTableCol.append('center').append('table');
+	// 	
+	// 	for(var j=0;j<Graph.Nodes.length;++j)
+	// 	{
+	// 		if(Graph.Nodes[j].Domain.length == 0)
+	// 			continue;
+	// 		
+	// 		var row = table.append('tr');
+	// 		row.append('td').append('p').text(Graph.Nodes[j].Name).style('margin', 0).style('text-align', 'left');
+	// 		var select = row.append('td').append('select').style('width', "100%");
+	// 		
+	// 		select.attr('id', "genflipseq-" + ends[i].name + Graph.Nodes[j].Name);
+	// 		for(var k=0;k<Graph.Nodes[j].Domain.length;++k)
+	// 			select.append('option').attr('value', Graph.Nodes[j].Domain[k]).text(Graph.Nodes[j].Domain[k]);
+	// 		select.attr('value', Graph.Nodes[j].Domain[0]);
+	// 	}
+	// }
+
+	ShowMessageBox(div.html(), ["Cancel", "Okay"], function(b) {
+		if(b !== "Okay")
+			return;
+			
+		// Generate query XML
+		var QueryXML = "<?xml version='1.0' encoding='us-ascii'?>\n<PREFERENCE-QUERY>\n";
+		QueryXML +=    "  <PREFERENCE-SPECIFICATION-FILENAME>%PREF_SPEC_NAME%</PREFERENCE-SPECIFICATION-FILENAME>\n"
+		QueryXML +=    "  <QUERY-TYPE>DOMINANCE</QUERY-TYPE>\n";
+		
+		for(var i=0;i<ends.length;++i)
+		{
+			QueryXML += "  <OUTCOME>\n";
+			QueryXML += "    <LABEL>" + ends[i].toUpperCase() + "</LABEL>\n";
+			
+			for(var j=0;j<Graph.Nodes.length;++j)
+			{
+				QueryXML += "    <ASSIGNMENT>\n";
+				QueryXML += "      <PREFERENCE-VARIABLE>" + Graph.Nodes[j].Name + "</PREFERENCE-VARIABLE>\n";
+				QueryXML += "      <VALUATION>" + d3.select("#flipseqselector_node" + j + "_end" + i).node().value + "</PREFERENCE-VARIABLE>\n";
+				QueryXML += "    </ASSIGNMENT>\n";
+			}
+			
+			QueryXML += "  </OUTCOME>\n";
+			QueryXML += "</PREFERENCE-QUERY>\n";
+		}
+		
+		var GraphXML = GraphToXML(Graph.Nodes);
+		// TODO: Some kind of loading message box here
+		dtstar.generateProof(QueryXML, GraphXML, function(contents) {
+			var flippingSequence = XMLToFlippingSequence(contents, Graph.Nodes);
+			
+			if(flippingSequence && flippingSequence.constructor === Array)
+			{
+				StartFlippingSequence(flippingSequence);
+			}
+			else
+			{
+				if(!flippingSequence) flippingSequence = "Unable to load flipping sequence file due to bad XML.";
+				ShowMessageBox("Error loading flipping sequence:<br>" + flippingSequence);
+			}
+		});
+	});
+}
 
 function LoadFlippingSequenceXMLFromFile()
 {
